e4b206ccbfe9a9ba3e3697e5d34c58bde372482f
yashraj-gs <rajyash0502@gmail.com>
2026-02-22 01:04:17 +0530

    UC2: Banner printing for OOPS implemented

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,29 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * This program prints the word OOPS in a large banner using
+ * ASCII art made from '*' and spaces.
+ *
+ * @author Developer
+ * @version 2
+ */
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Line 1
+        System.out.println(" ***     ***     *****     *****  ");
+        // Line 2
+        System.out.println("*   *   *   *    *   *     *     ");
+        // Line 3
+        System.out.println("*   *   *   *    *****     ****  ");
+        // Line 4
+        System.out.println("*   *   *   *    *             * ");
+        // Line 5
+        System.out.println(" ***     ***     *         *****  ");
+        // Line 6
+        System.out.println();
+        
     }
 }
